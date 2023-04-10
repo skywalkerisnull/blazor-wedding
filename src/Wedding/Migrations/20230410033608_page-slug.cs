661d3517e35f654b13f0cafe63d21d2f746c69d3
Ashley Walker <7260106+skywalkerisnull@users.noreply.github.com>
2023-04-10 15:15:22 +1000

    tons of smalle updates

---
+++ src/Wedding/Migrations/20230410033608_page-slug.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wedding.Migrations
+{
+    /// <inheritdoc />
+    public partial class pageslug : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Slug",
+                table: "Pages",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pages_Slug",
+                table: "Pages",
+                column: "Slug",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Pages_Slug",
+                table: "Pages");
+
+            migrationBuilder.DropColumn(
+                name: "Slug",
+                table: "Pages");
+        }
+    }
+}
