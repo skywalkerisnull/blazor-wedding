3ecd64d975f2e499c67f87f6abd0461d8c2f305c
Ashley Walker <7260106+skywalkerisnull@users.noreply.github.com>
2023-04-15 21:24:32 +1000

    QR Code Generator

---
+++ src/Wedding/Services/QrCodeGenerator.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Wedding.Services
+{
+    public static class QrCodeGenerator
+    {
+        public static Bitmap GenerateQRCode(string url)
+        {
+            // https://github.com/manuelbl/QrCodeGenerator
+        }
+    }
+}
