f55051b548fb5a5c1cf5b134263cb9cd9b7cc86b
Ashley Walker <7260106+skywalkerisnull@users.noreply.github.com>
2023-04-16 21:12:12 +1000

    Fixed the logout not finding the JS

---
+++ src/Wedding/wwwroot/js/logout.js
@@ -0,0 +1,8 @@
+﻿function logout() {
+    fetch("/Identity/Account/Logout", { method: "POST" })
+        .then(response => {
+            if (response.ok) {
+                window.location.href = "/login";
+            }
+        });
+}
