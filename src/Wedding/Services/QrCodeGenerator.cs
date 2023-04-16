098501849dafc4aa034c80f597878bc96747a717
Ashley Walker <7260106+skywalkerisnull@users.noreply.github.com>
2023-04-16 10:28:24 +1000

    Updated navigation

--- src/Wedding/Services/QrCodeGenerator.cs
+++ src/Wedding/Services/QrCodeGenerator.cs
@@ -3,9 +3,9 @@ namespace Wedding.Services
 {
     public static class QrCodeGenerator
     {
-        public static Bitmap GenerateQRCode(string url)
-        {
-            // https://github.com/manuelbl/QrCodeGenerator
-        }
+        //public static Bitmap GenerateQRCode(string url)
+        //{
+        //    // https://github.com/manuelbl/QrCodeGenerator
+        //}
     }
 }
