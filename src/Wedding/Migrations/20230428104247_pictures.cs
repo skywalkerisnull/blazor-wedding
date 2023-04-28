f1567250fd863a1111917b820462202321ce9603
Ashley Walker <7260106+skywalkerisnull@users.noreply.github.com>
2023-04-28 20:43:06 +1000

    DB updates

---
+++ src/Wedding/Migrations/20230428104247_pictures.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wedding.Migrations
+{
+    /// <inheritdoc />
+    public partial class pictures : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "PictureId",
+                table: "Guests",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Pictures",
+                columns: table => new
+                {
+                    PictureId = table.Column<Guid>(type: "uuid", nullable: false),
+                    OriginalFileName = table.Column<string>(type: "text", nullable: false),
+                    FileHash = table.Column<byte[]>(type: "bytea", nullable: false),
+                    DateTimeUploadedUtc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    FileName = table.Column<string>(type: "text", nullable: false),
+                    FilePath = table.Column<string>(type: "text", nullable: false),
+                    FileUrl = table.Column<string>(type: "text", nullable: false),
+                    FileDescription = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pictures", x => x.PictureId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Guests_PictureId",
+                table: "Guests",
+                column: "PictureId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Guests_Pictures_PictureId",
+                table: "Guests",
+                column: "PictureId",
+                principalTable: "Pictures",
+                principalColumn: "PictureId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Guests_Pictures_PictureId",
+                table: "Guests");
+
+            migrationBuilder.DropTable(
+                name: "Pictures");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Guests_PictureId",
+                table: "Guests");
+
+            migrationBuilder.DropColumn(
+                name: "PictureId",
+                table: "Guests");
+        }
+    }
+}
